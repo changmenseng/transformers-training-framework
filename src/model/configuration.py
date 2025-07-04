39e71923ae59008e5cd0c757eaebbe1478283619
changmenseng <60961806+changmenseng@users.noreply.github.com>
2025-07-04 09:12:53 +0800

    Update configuration.py

--- src/model/configuration.py
+++ src/model/configuration.py
@@ -1,1 +1,13 @@
+from transformers import GPT2Config
 
+class GPT2ClassifierConfig(GPT2Config):
+    model_type = "gpt2_classifier"
+    
+    def __init__(
+        self,
+        n_classes=3,
+        sentence_pooling_method='mean',
+        **kwargs
+    ):
+        self.n_classes = n_classes
+        super().__init__(**kwargs)
